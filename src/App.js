bee9fd5a9d5ed2651ca68604dea6de5bf25421e0
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-04-15 15:32:57 +0100

    skills added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import About from "./components/About"
 import Home from "./components/Home"
+import Skills from"./components/Skills"
 function App() {
   return (
     <div>
+ 
       <Home/>
       <About/>
+      <Skills/>
     </div>
   );
 }
