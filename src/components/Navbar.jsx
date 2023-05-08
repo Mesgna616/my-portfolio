d5f6f552c49bcc478bb36cc240f4a1ae8c30c176
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-05-08 12:19:05 +0100

    logo png

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,6 @@
 import React, {useState} from "react";
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from "react-icons/fa";
+import {HiOutlineMail} from "react-icons/hi";
+import {BsFillPersonLinesFill} from "react-icons/bs";
+import LogoNew from "../assets/logoNew.png";
+import {Link} from "react-scroll";
