fb9f8590280cde57bdaffb838bce1754af6d8987
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-05-08 13:58:59 +0100

    tailwind config

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-auto">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-10">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               About
@@ -13,17 +13,15 @@ const About = () => {
           <div></div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className=" sm:text-left text-4xl font-bold">
+          <div className=" sm:text-left text-2xl font-bold">
             <p>
-              A passionate full stack developer 
+              A passionate full stack developer with solid foundation both on back end and front end Technologies. 
               {" "}
             </p>
           </div>
           <div>
-            <p className="text-2xl">
-              I am a recent graduate with hand on experience in web development. I have gained experience both in front-end and back-end technologies.
-               Core technical skills including JavaScript, React,
-              Node and SQL/NoSQL databases. I embarking to learn cloud technologies.
+            <p className=" text-xl ">
+            A junior software developer committed to continuous learning, receptive to feedback, prioritizing clients, and service-centric approach, striving to exceed expectations through effective communication, collaboration, and teamwork. A quick learner with enthusiasm for modern technology and a solid foundation in programming principles..
             </p>
           </div>
         </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,13 +5,13 @@ import back3 from "../assets/back3.png"
 
 const Work = () => {
     return (
-      <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f] margin-bottom: 10px;">
         <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
           <div className="py-8">
             <p className="text-4xl font-bold inline border-b-4  text-gray-300 border-pink-600">
               Work
             </p>
-            <p className="py-6">Check out some of my projects</p>
+            <p className="py-6 text-4xl">Projects completed </p>
           </div>
   
           {/* Container */}
