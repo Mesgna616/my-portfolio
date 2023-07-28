d111094747b9026860bd25cd3b767c85b48463fc
mesgna woldeb <mesgna.mehari8@gmail.com>
2023-07-28 14:14:28 +0100

    resume redone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300 "
-              href="https://drive.google.com/drive/u/0/my-drive"
+              href="https://drive.google.com/file/d/1-5vc_tdt_1-6gJ-nL7FiJjeqkOaWn-t5/view"
               target="_blank"
               rel="noreferrer"
             >
